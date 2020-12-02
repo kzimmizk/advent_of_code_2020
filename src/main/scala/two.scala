49f8f0daacdb695f7e1564be1d8439ed29a5b2cc
Kristopher Zimmerman <kristopherzimmerman@gatech.edu>
2020-12-02 16:25:11 -0700

    Day2 Part2

--- src/main/scala/two.scala
+++ src/main/scala/two.scala
@@ -1,29 +1,27 @@
 import scala.io.Source
+import scala.util.matching.Regex
 
 /**
   * Created by kzimmerman on 12/2/20.
   */
 object two {
-  val filename = "./src/main/data/2a"
-  val line = raw"(\d+)-(\d+) ([a-z]{1}): ([a-z]*)".r
-  val data = Source.fromFile(filename).getLines().map {
+  val filename: String = "./src/main/data/2a"
+  val line: Regex = raw"(\d+)-(\d+) ([a-z]{1}): ([a-z]*)".r
+  val data: List[Password] = Source.fromFile(filename).getLines().map {
     case line(minCount, maxCount, character, password) =>
       Password(minCount.toInt, maxCount.toInt, character.toCharArray.head, password)
-  }
+  }.toList
 
-  case class Password(minCount: Int, maxCount: Int, character: Char, password: String) {
-    val count: Int = password.count(_ == character)
-    val valid: Boolean = (minCount <= count) &&  (maxCount >= count)
+  case class Password(a: Int, b: Int, char: Char, password: String) {
+    val count: Int = password.count(_ == char)
+    val validRule1: Boolean = (a <= count) &&  (b >= count)
+    val validRule2: Boolean = (password.charAt(a-1) == char) ^ (password.charAt(b-1) == char)
   }
 
   def main(args: Array[String]): Unit = {
-
-    val firstAnswer: Int = data.count(_.valid)
-
-    val secondAnswer: Int = 0
-
+    val firstAnswer: Int = data.count(_.validRule1)
+    val secondAnswer: Int = data.count(_.validRule2)
     println(s"First Answer: valid password count is $firstAnswer")
-
-    data.foreach(println)
+    println(s"Second Answer: valid password count is $secondAnswer")
   }
 }
